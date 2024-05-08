510374518c33356bc5a0cb29b972cd63f6940f15
hoan02 <hoanvipboi1@gmail.com>
2024-05-08 12:47:34 +0700

    fix missing connectToDB

--- src/app/api/cron/route.ts
+++ src/app/api/cron/route.ts
@@ -1,7 +1,9 @@
 import { NextResponse } from "next/server";
 import User from "@/lib/models/user.model";
+import connectToDB from "@/lib/mongodb/mongoose";
 
 export async function GET() {
+  await connectToDB();
   const users = await User.find();
   for (const user of users) {
     await user.updatePremiumState();
