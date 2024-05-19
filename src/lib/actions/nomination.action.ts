893d12dafff6b312a31341eccacfc1f6bbe32219
hoan02 <hoanvipboi1@gmail.com>
2024-05-19 09:36:18 +0700

    update handle MonthlyStats

--- src/lib/actions/nomination.action.ts
+++ src/lib/actions/nomination.action.ts
@@ -1,52 +1,76 @@
 "use server";
 
-import { revalidatePath } from "next/cache";
 import { auth } from "@clerk/nextjs/server";
 
 import connectToDB from "@/lib/mongodb/mongoose";
 import Nomination from "../models/nomination.model";
 import Novel from "../models/novel.model";
+import MonthlyStats from "../models/monthlyStats.model";
+
+const getCurrentDateDetails = () => {
+  const currentDate = new Date();
+  return {
+    month: currentDate.getMonth(),
+    year: currentDate.getFullYear(),
+  };
+};
+
+const dbConnection = connectToDB();
 
 export const createNomination = async (novelSlug: string) => {
+  const { month, year } = getCurrentDateDetails();
+
   try {
     const { userId } = auth();
-    await connectToDB();
-    await Nomination.create({
-      clerkId: userId,
-      novelSlug,
-    });
-    await Novel.findOneAndUpdate(
-      {
-        novelSlug,
-      },
-      { $inc: { nominationCount: 1 } }
+    await dbConnection;
+
+    const [nomination, novelUpdate] = await Promise.all([
+      Nomination.create({ clerkId: userId, novelSlug }),
+      Novel.findOneAndUpdate(
+        { novelSlug },
+        { $inc: { nominationCount: 1 } },
+        { new: true, upsert: true }
+      ),
+    ]);
+
+    await MonthlyStats.findOneAndUpdate(
+      { month, year, clerkId: novelUpdate.uploader },
+      { $inc: { nominationCount: 1 } },
+      { new: true, upsert: true }
     );
-    revalidatePath("/");
+
     return { success: true, message: "Đề cử thành công!" };
   } catch (error) {
-    console.error(error);
+    console.error("Create Nomination Error:", error);
     throw new Error("Không thể đề cử!");
   }
 };
 
 export const deleteNomination = async (novelSlug: string) => {
+  const { month, year } = getCurrentDateDetails();
+
   try {
     const { userId } = auth();
-    await connectToDB();
-    await Nomination.findOneAndDelete({
-      clerkId: userId,
-      novelSlug,
-    });
-    await Novel.findOneAndUpdate(
-      {
-        novelSlug,
-      },
-      { $inc: { nominationCount: -1 } }
+    await dbConnection;
+
+    const [nominationDeletion, novelUpdate] = await Promise.all([
+      Nomination.findOneAndDelete({ clerkId: userId, novelSlug }),
+      Novel.findOneAndUpdate(
+        { novelSlug },
+        { $inc: { nominationCount: -1 } },
+        { new: true }
+      ),
+    ]);
+
+    await MonthlyStats.findOneAndUpdate(
+      { month, year, clerkId: novelUpdate.uploader },
+      { $inc: { nominationCount: -1 } },
+      { new: true }
     );
-    revalidatePath("/");
+
     return { success: true, message: "Xóa đề cử thành công!" };
   } catch (error) {
-    console.error(error);
+    console.error("Delete Nomination Error:", error);
     throw new Error("Không thể xóa đề cử!");
   }
 };
