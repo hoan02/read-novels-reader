dcd88cbeeea4d09f5ebf40c4270f26788947446b
hoan02 <hoanvipboi1@gmail.com>
2024-06-23 10:48:11 +0700

    update banner

--- src/components/layouts/Banner.tsx
+++ src/components/layouts/Banner.tsx
@@ -1,9 +1,15 @@
+import Image from "next/image";
+
 const Banner = () => {
   return (
-    <div
-      className="absolute z-0 left-0 overflow-hidden w-full h-96 bg-cover bg-no-repeat bg-center"
-      style={{ backgroundImage: "url('/banner.webp')" }}
-    ></div>
+    <div className="absolute z-0 left-0 overflow-hidden w-full h-96 bg-cover bg-no-repeat bg-center">
+      <Image
+        fill
+        className="object-center object-cover pointer-events-none"
+        src="/banner.webp"
+        alt="banner"
+      />
+    </div>
   );
 };
 
