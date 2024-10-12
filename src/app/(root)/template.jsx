48292db5530cb65a9f2aec6646c67c80585c69aa
HoanIT <lehoan.dev@gmail.com>
2024-10-12 18:52:20 +0700

    Update dependencies to latest versions

--- src/app/(root)/template.jsx
+++ src/app/(root)/template.jsx
@@ -7,7 +7,7 @@ export default function Template({ children }) {
     <>
       {children}
       <Script
-        src="https://sf-cdn.coze.com/obj/unpkg-va/flow-platform/chat-app-sdk/0.1.0-beta.4/libs/oversea/index.js"
+        src="https://sf-cdn.coze.com/obj/unpkg-va/flow-platform/chat-app-sdk/0.1.0-beta.5/libs/oversea/index.js"
         strategy="lazyOnload"
         onLoad={() => {
           if (window.CozeWebSDK) {
